a925f4b6d7fda20f27970528f70961eb6257d2f0
Preeti Ghatnekar <52157348+GhatnekarPreeti@users.noreply.github.com>
2022-11-25 17:36:28 +0530

    JIRA-3 : Added new folder to code

---
+++ src/java/Interfaces/NewInterface.java
@@ -0,0 +1,6 @@
+package Interfaces;
+
+public interface NewInterface extends javax.ejb.EJBLocalHome {
+    double pi = 3.14;
+    int valuefortheNew_fieldcreated = 1;
+}
---
+++ src/java/Vehicle.java
@@ -0,0 +1,15 @@
+
+
+public abstract class Vehicle extends Object {
+    int isValid;
+    public abstract void setCategory(String category, int category_id);
+
+    public Object couldBeAbstract() {
+        return null;
+    }
+
+    int isValid() {
+        return 1;
+    }
+}
+
