4ca8e25bc6cfaf442d7a4183642277c30bdd1420
Manasi1313 <76029071+Manasi1313@users.noreply.github.com>
2022-11-29 14:23:48 +0530

    Update MyClass.java

--- src/java/MyClass.java
+++ src/java/MyClass.java
@@ -10,6 +10,7 @@ public class MyClass implements SessionBean {
         try{
             int a=10, b=0, c=0;
             c=(a/b);
+            System.out.println("Value:" + c);
         } catch(Exception e){
         
         }
