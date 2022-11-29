eaafd3952ef9496df59f8069936ea65e6360d7d8
Manasi1313 <76029071+Manasi1313@users.noreply.github.com>
2022-11-29 14:38:47 +0530

    JIRA-16: 4 code issues added

--- src/java/MyClass.java
+++ src/java/MyClass.java
@@ -17,6 +17,10 @@ public class MyClass implements SessionBean {
             System.out.println("Value:" + c);
             System.out.println("Value:" + c);
             System.out.println("Value:" + c);
+             System.out.println("Value:" + c);
+             System.out.println("Value:" + c);
+             System.out.println("Value:" + c);
+             System.out.println("Value:" + c);
         } catch(Exception e){
         
         }
