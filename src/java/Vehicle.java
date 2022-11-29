ab3897fa30f21039d581f6a5d0f870883b1fc8de
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2022-11-29 15:07:30 +0530

    JIRA-11 : added code issue 1

--- src/java/Vehicle.java
+++ src/java/Vehicle.java
@@ -7,6 +7,20 @@ public abstract class Vehicle extends Object {
     public Object couldBeAbstract() {
         return null;
     }
+    
+    public boolean getCategoryId() {
+        try {
+            String txtA = "Category";
+            boolean flag = false;
+            if (categoryid > 0)
+                flag = true;
+            return flag;
+        } catch (IllegalArgumentException e) {
+            throw e;
+        } catch (IllegalStateException e) {
+            throw e;
+        }
+    }
 
     int isValid() {
         return 1;
